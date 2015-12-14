5edfe1e22ab67decc3e24de8aec8add3c00534e2
ChengHsuanLiu <chenghsuango@gmail.com>
2015-12-14 13:38:28 +0800

    update

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   get 'house' => 'pages#house'
   get 'new' => 'pages#new'
   get 'register' => 'pages#register'
+  get 'user' => 'pages#user'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
