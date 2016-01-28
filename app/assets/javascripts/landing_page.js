3e17461f20f3e637ae876954a97737015345886c
ChengHsuanLiu <chenghsuango@gmail.com>
2016-01-28 12:04:21 +0800

    update

---
+++ app/assets/javascripts/landing_page.js
@@ -0,0 +1,1 @@
+//= require ./theta/theta-all.js
---
+++ app/assets/javascripts/theta/theta-all.js
@@ -0,0 +1,6 @@
+//= require ./modernizr-2.7.1-csstransforms3d-canvas-touch.js
+//= require ./megapix-image.js
+//= require ./three.min.js
+//= require ./Detector.js
+//= require ./CSS3DRenderer.js
+//= require ./theta-viewer.min.js
